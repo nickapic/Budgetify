ef54adbb479b528969e3d1a19f6bff10775a6f0a
Aniket Chauhan <43927184+nickapic@users.noreply.github.com>
2020-03-20 22:54:09 +0200

    Some basic stuff changed

--- server.js
+++ server.js
@@ -54,6 +54,13 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 //Limit requests from same IP address to stop DDOS attacks
 const limiter = rateLimit({
   max: 500,
@@ -74,12 +81,6 @@ app.all("*", (req, res, next) => {
 //Error handling middleware
 app.use(globalErrorHandeler);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
