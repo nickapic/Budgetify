bc8eab05b4a5d13aa08c0c9c9669161a725d06a2
Aniket Chauhan <43927184+nickapic@users.noreply.github.com>
2020-03-21 16:46:16 +0200

    Build done

---
+++ client/build/precache-manifest.8ff163a38de9afa3cd8ca97a90410a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4519946dfd012d69ac1709f038e9963b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b494b6168edffb2e3cff",
+    "url": "/static/css/main.e249d344.chunk.css"
+  },
+  {
+    "revision": "1615c13839d1ae098b72",
+    "url": "/static/js/2.ad0dfc0a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.ad0dfc0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b494b6168edffb2e3cff",
+    "url": "/static/js/main.3ae5d850.chunk.js"
+  },
+  {
+    "revision": "f0292414833a773321f8",
+    "url": "/static/js/runtime-main.5e8fde42.js"
+  }
+]);
