c7662c56eec347f5d6f0d9258b45becf3a7c9459
Aniket Chauhan <43927184+nickapic@users.noreply.github.com>
2020-03-23 21:53:35 +0200

    Trying to fix stuff here

--- server.js
+++ server.js
@@ -63,6 +63,15 @@ const limiter = rateLimit({
 
 app.use("/api", limiter);
 
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //Just our normal routes
 app.use("/api/v1/transactions", transactions);
 app.use("/api/v1/user", users);
