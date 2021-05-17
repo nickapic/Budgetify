88bd7354e485ff15fac056fd5b07b1965601a49f
nickapic <43927184+nickapic@users.noreply.github.com>
2021-05-17 18:22:26 +0300

    removed a test

--- client/cypress/integration/homepage.spec.js
+++ client/cypress/integration/homepage.spec.js
@@ -6,10 +6,3 @@ describe("Rendered the Hero Page and SVG accurately",() =>{
     })
 });
 
-describe("Rendered the login page and had the form",() =>{
-    it("renders correctly", () =>{
-        cy.visit('/')
-        cy.get(".hero-section").should("exist")
-        cy.get(".hero-section-left").should("exist")
-    })
-});
